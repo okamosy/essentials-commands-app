54da5552d1a05bbe3ccc68fc79ff89490d6238aa
Seiji Okamoto <okamosy@gmail.com>
2013-02-10 14:13:33 +0000

    Fixed problem introduced by changing uri_request config.  form action is now hard-coded

--- application/views/login.php
+++ application/views/login.php
@@ -2,9 +2,9 @@
 	<h1>Login</h1>
 	<?php echo $errors = validation_errors() ? '<ul class="error">'.$errors.'</ul>' : ''; ?>
 	<?php echo isset($login_msg) ? '<div class="error">'.$login_msg.'</div>' : ''; ?>
-	<?php echo form_open('login'); ?>
+	<form action="<?php echo base_url('login'); ?>" method="post" accept-charset="utf-8">
 		<input type="text" name="username" id="username" value="<?php echo set_value('username'); ?>" required autofocus />
 		<input type="password" name="password" id="password" required />
 		<input type="submit" value="Login" class="button" />
-	<?php echo form_close(); ?>
+	</form>
 </article>
