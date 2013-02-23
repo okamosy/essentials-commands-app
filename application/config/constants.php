2ce999a4cf355ea3cdc908c1efff4db489126942
Seiji Okamoto <okamosy@gmail.com>
2013-02-23 18:31:32 +0000

    Increased maximum number of search results to 8

--- application/config/constants.php
+++ application/config/constants.php
@@ -44,7 +44,7 @@ define('ESS_PROMOTED',    'promoted');
 define('ESS_DEFAULT',     'default');
 
 // Define the maximum number of search results the API will return
-define('MAX_SEARCH_RESULTS', 6);
+define('MAX_SEARCH_RESULTS', 8);
 
 // Define Ajax Request
 define('IS_AJAX', isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest');
